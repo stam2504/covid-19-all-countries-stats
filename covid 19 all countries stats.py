dbaa9eb9f50694a4b5daec82022386390c40db14
Stamatis Gounidis <74081279+stam2504@users.noreply.github.com>
2022-01-26 03:27:24 +0200

    Update covid 19 all countries stats.py

--- covid 19 all countries stats.py
+++ covid 19 all countries stats.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import matplotlib as plt
-complete_df=pd.read_csv('https://covid.ourworldindata.org/data/ecdc/full_data.csv')
+complete_df=pd.read_csv('https://raw.githubusercontent.com/stam2504/covid-19-all-countries-stats/main/full_data.csv')
 complete_df.iloc[[0,1250,9500,9501,20500]]
 today=complete_df[complete_df['date']=='2020-06-04']
 today_more_than_100k=today[today['total_cases'] >= 100000]
